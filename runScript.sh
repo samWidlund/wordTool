7ded5a5f291e7c3bb98e9eacc876ab78d3c9c12a
samWidlund <sam.wid415@gmail.com>
2025-05-06 22:19:01 +0200

    make shell script dynamic

--- runScript.sh
+++ runScript.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gnome-terminal -- bash -c "/home/samuel-widlund/repos/wordTool/venv/bin/python /home/samuel-widlund/repos/wordTool/script.py; echo; read -p 'press[Enter]ToClose'"
 
+SCRIPT_DIR="$(dirname "$0")"
+VENV_PYTHON="$SCRIPT_DIR/venv/bin/python"
+SCRIPT_FILE="$SCRIPT_DIR/script.py"
+
+gnome-terminal -- bash -c "$VENV_PYTHON $SCRIPT_FILE; echo; read -p 'press [Enter] to close'"
