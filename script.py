e3ab282f6d5103b80c97b4b620664fe1c97f3de0
samWidlund <sam.wid415@gmail.com>
2025-05-07 00:07:26 +0200

    add definition api

--- script.py
+++ script.py
@@ -1,4 +1,3 @@
-
 import requests
 import os
 from dotenv import load_dotenv
@@ -6,35 +5,53 @@ from dotenv import load_dotenv
 load_dotenv()
 
 apiKey = os.getenv('API_KEY')
+process = True
+header = {'X-Api-Key': apiKey}
+
 if not apiKey:
     print("API key not found. Please set the API_KEY environment variable.")
     exit(1)
 
-process = True
 
 while process:
-    word = input("inputWord: ")
-    api_url = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(word)
+    word = input("inputWord:")
+    print("\n") #blankspace
 
-    response = requests.get(api_url, headers={'X-Api-Key': apiKey})
-    if response.status_code == requests.codes.ok:
-        data = response.json()
-        
+    thesaurusUrl = 'https://api.api-ninjas.com/v1/thesaurus?word={}'.format(word)
+    dictionaryUrl = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+    response1 = requests.get(thesaurusUrl, headers=header)
+    response2 = requests.get(dictionaryUrl, headers=header)
+    
+    if response1.status_code == requests.codes.ok and response2.status_code == requests.codes.ok:
+        data1 = response1.json()
+        data2 = response2.json()
 
-        synonyms = data.get('synonyms', [])
-        antonyms = data.get('antonyms', [])
+        definition = data2[0]['meanings'][0]['definitions'][0]['definition'] # definition example
+        synonyms = data1.get('synonyms', [])
+        antonyms = data1.get('antonyms', [])
 
-        print("word:\n", word)
+        if not definition:
+            print("definition:\n", "no definition found")
+            continue #unsure?
+        else:
+            print("definition:\n", definition)
         print("\n")
-        print("synonyms:\n", synonyms)
+        if not synonyms:
+            print("synonyms:\n", "no synonyms found")
+        else:
+            print("synonyms:\n", synonyms) 
         print("\n")
-        print("antonyms:\n", antonyms)
+        if not antonyms:
+            print("antonyms:\n", "no antonyms found")   
+        else:
+            print("antonyms:\n", antonyms)
         print("\n")
     else:
-        print("Error:", response.status_code, response.text)
+        print("error:", response1.status_code, response1.text, "\nplease enter a valid english word\n")
     
+    # end or continue
     contiue = input("anotherWord? (y/n) ")
-
+    print("\n")
     if contiue.lower() == 'n':
         process = False
     elif contiue.lower() == 'y':
